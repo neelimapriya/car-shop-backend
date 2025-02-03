54b4de6bcd6636fddeb1cf835e639232f26cfa58
Nelima sultana <neelimasultana6@gmail.com>
2025-02-03 12:31:15 +0600

    add errors and interface

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express()
 
 // parser or middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:['http://localhost:5173'], credentials:true}));
 
 
 
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import app from './app';
 import mongoose from 'mongoose';
 import config from './app/config';
+import { Server } from 'http';
 
+let server:Server
 async function main() {
   try {
     await mongoose.connect(config.DATABASE_URL as string);
 
-    app.listen(config.port, () => {
+    server=app.listen(config.port, () => {
       console.log(` App listening on port ${config.port}`);
     });
   } catch (err) {
